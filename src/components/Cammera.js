29a8a063368dcbd52f92e252544952a6385d190f
maicom goncalves <gonm202@gmail.com>
2021-05-21 10:34:47 -0400

    adicionando codigo de barras

---
+++ src/components/Cammera.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Scanner from './Scanner';
+import ReactDOM from "react-dom";
+
+function Cammera() {
+  const [camera, setCamera] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const onDetected = result => {
+    setResult(result);
+  };
+
+  return (
+    <div className="camera">
+      <p>{result ? result : "Scanning..."}</p>
+      <button onClick={() => setCamera(!camera)}>
+        {camera ? "Stop" : "Start"}
+      </button>
+      <div className="container">
+        {camera && <Scanner onDetected={onDetected} />}
+      </div>
+    </div>
+  );
+}
+export default Cammera;
---
+++ src/services/BarcodeService.js
@@ -0,0 +1,12 @@
+//import axios from 'axios';
+import Barcode from '../pages/main'
+import { codigoDeBarras } from '../pages/main'
+export default class ListaService {
+    constructor() {
+        this.Barcode = '';
+        codebr = codigoDeBarras;
+        console.log(codebr + 'teste testado')
+    }
+ 
+
+}
